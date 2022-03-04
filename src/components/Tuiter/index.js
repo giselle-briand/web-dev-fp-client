2e69673cc745be574d14573c9a33ac9ad7c998aa
gisellebriand <briand.g@husky.neu.edu>
2022-03-04 15:29:07 -0500

    a6 React.js sp22

--- src/components/Tuiter/index.js
+++ src/components/Tuiter/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import '../../css/universal.css';
 import '../../css/explore.css';
+import './vendors/bootstrap/css/bootstrap.min.css';
+import './vendors/bootstrap/bootstrap.min.css';
+import './vendors/fontawesome/css/all.min.css';
+
 import ExploreScreen from "./ExploreScreen/ExploreScreen";
 import {Link} from "react-router-dom";
 const Tuiter = () => {
