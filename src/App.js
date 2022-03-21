9ac963d7e7d454659ef31ab953eab7880b15edc8
gisellebriand <briand.g@husky.neu.edu>
2022-03-21 16:14:32 -0400

    debug tuiter/home mistake

--- src/App.js
+++ src/App.js
@@ -15,18 +15,12 @@ function App() {
           <div className="container">
               <Routes>
                   <Route path="/">
-                      <Route index element={<Labs/>} />
-                      <Route path="/labs"
-                             // exact={true}
-                             element={<Labs/>}/>
-                      <Route path="/hello"
-                             element={<HelloWorld/>}/>
-                      <Route path="/tuiter"
-                             element={<Tuiter/>}>
-                          <Route index
-                                 element={<HomeScreen/>}/>
-                          <Route path="explore"
-                                 element={<ExploreScreen/>}/>
+                      <Route index element={<Labs/>}/>
+                      <Route path="labs" exact={true} element={<Labs/>}/>
+                      <Route path="hello" element={<HelloWorld/>}/>
+                      <Route path="tuiter/" element={<Tuiter/>}>
+                          <Route index element={<HomeScreen/>}/>
+                          <Route path="explore" element={<ExploreScreen/>}/>
                       </Route>
                   </Route>
               </Routes>
