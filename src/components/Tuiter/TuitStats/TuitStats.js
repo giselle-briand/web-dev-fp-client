26006455edeb7ea166b5018461c7d649db323e32
gisellebriand <briand.g@husky.neu.edu>
2022-03-31 20:30:45 -0400

    a8 REST sp22

--- src/components/Tuiter/TuitStats/TuitStats.js
+++ src/components/Tuiter/TuitStats/TuitStats.js
@@ -1,27 +1,66 @@
 import {useDispatch} from "react-redux";
 import React from "react";
+import {updateTuit} from "../actions/tuits-actions";
 
 const TuitStats = ({tuit}) => {
     const dispatch = useDispatch();
-    const likeTuit = () => {
-        dispatch({type: 'like-tuit', tuit});
-    };
     return (
-        <div className="wd-float-done">
+        <div className="wd-float-done wd-stats col-12">
             <div className="wd-float-left wd-tuit-icon-spacing-3">
-                <i className="fa fa-comment wd-icon-spacing-2"/>{tuit.stats.comments}
+                <i className="fa fa-comment wd-icon-spacing-2"/>
+                {tuit.comments}
             </div>
             <div className="wd-float-left wd-tuit-icon-spacing-3">
-                <i className="fa-solid fa-retweet wd-icon-spacing-2"/>{tuit.stats.retuits}
+                <i className="fa-solid fa-retweet wd-icon-spacing-2"/>
+                {tuit.retuits}
             </div>
-            <span className="wd-float-left wd-tuit-icon-spacing-3" onClick={likeTuit}>
+            <span className="wd-float-left wd-tuit-icon-spacing-3"
+                  onClick={() => {
+                      if (tuit.liked === true) {
+                          updateTuit(dispatch, {
+                              ...tuit,
+                              likes: tuit.likes - 1,
+                              liked: false
+                          })
+                      } else {
+                          updateTuit(dispatch, {
+                              ...tuit,
+                              likes: tuit.likes + 1,
+                              liked: true
+                          })
+                      }
+                  }}>
                 {
-                    tuit.liked && <i className="fas fa-heart wd-icon-spacing-2" style={{color: 'tomato'}}/>
+                    tuit.liked && <i className="fa fa-thumbs-up ms-2 wd-icon-spacing-2"/>
                 }
                 {
-                    !tuit.liked && <i className="far fa-heart wd-icon-spacing-2"/>
+                    !tuit.liked && <i className="far fa-thumbs-up ms-2 wd-icon-spacing-2"/>
                 }
-                {tuit.stats && tuit.stats.likes}
+                {tuit.likes}
+            </span>
+            <span className="wd-float-left wd-tuit-icon-spacing-3"
+                  onClick={() => {
+                      if (tuit.disliked === true) {
+                          updateTuit(dispatch, {
+                              ...tuit,
+                              dislikes: tuit.dislikes - 1,
+                              disliked: false
+                          })
+                      } else {
+                          updateTuit(dispatch, {
+                              ...tuit,
+                              dislikes: tuit.dislikes + 1,
+                              disliked: true
+                          })
+                      }
+                  }}>
+                {
+                    tuit.disliked && <i className="fa fa-thumbs-down ms-2 wd-icon-spacing-2"/>
+                }
+                {
+                    !tuit.disliked && <i className="far fa-thumbs-down ms-2 wd-icon-spacing-2"/>
+                }
+                {tuit.dislikes}
             </span>
             <div className="wd-float-left wd-tuit-icon-spacing-3">
                 <i className="fa fa-share-square wd-icon-spacing-2"/>
--- src/components/Tuiter/reducers/tuits-reducer.js
+++ src/components/Tuiter/reducers/tuits-reducer.js
@@ -1,48 +1,61 @@
-import tuits from "../data/tuits.json";
+import {UPDATE_TUIT, CREATE_TUIT, DELETE_TUIT, FIND_ALL_TUITS} from "../actions/tuits-actions";
 
-const tuitsReducer =
-    (state = tuits, action) => {
+const tuitsReducer = (state = [], action) => {
         switch (action.type) {
-            case 'like-tuit':
-                return state.map(tuit => {
-                    if(tuit._id === action.tuit._id) {
-                        if(tuit.liked === true) {
-                            tuit.liked = false;
-                            tuit.stats.likes--;
-                        } else {
-                            tuit.liked = true;
-                            tuit.stats.likes++;
-                        }
-                        return tuit;
-                    } else {
-                        return tuit;
-                    }
-                });
-            case 'delete-tuit':
-                return state.filter(
-                    tuit => tuit._id !== action.tuit._id);
-            case 'create-tuit':
-                const newTuit = {
-                    tuit: action.tuit,
-                    _id: (new Date()).getTime() + '',
-                    postedBy: {
-                        "username": "ReactJS"
-                    },
-                    handle: "webdev",
-                    time: "just now",
-                    "avatar-image": "../../../media/profileimage.jpg",
-                    stats: {
-                        retuits: 111,
-                        likes: 222,
-                        replies: 333
-                    }
-                }
+            case FIND_ALL_TUITS:
+                return action.tuits;
+            case DELETE_TUIT:
+                return state.filter(tuit => tuit._id !== action.tuit._id);
+            case CREATE_TUIT:
                 return [
-                    newTuit,
-                    ...state,
+                    action.newTuit,
+                    ...state
                 ];
+            case UPDATE_TUIT:
+                return state.map(
+                    tuit => tuit._id === action.tuit._id ? action.tuit : tuit);
             default:
-                return tuits
+                return state;
+            // case 'like-tuit':
+            //     return state.map(tuit => {
+            //         if(tuit._id === action.tuit._id) {
+            //             if(tuit.liked === true) {
+            //                 tuit.liked = false;
+            //                 tuit.stats.likes--;
+            //             } else {
+            //                 tuit.liked = true;
+            //                 tuit.stats.likes++;
+            //             }
+            //             return tuit;
+            //         } else {
+            //             return tuit;
+            //         }
+            //     });
+            // case 'delete-tuit':
+            //     return state.filter(
+            //         tuit => tuit._id !== action.tuit._id);
+            // case 'create-tuit':
+            //     const newTuit = {
+            //         tuit: action.tuit,
+            //         _id: (new Date()).getTime() + '',
+            //         postedBy: {
+            //             "username": "ReactJS"
+            //         },
+            //         handle: "webdev",
+            //         time: "just now",
+            //         "avatar-image": "../../../media/profileimage.jpg",
+            //         stats: {
+            //             retuits: 111,
+            //             likes: 222,
+            //             replies: 333
+            //         }
+            //     }
+            //     return [
+            //         newTuit,
+            //         ...state,
+            //     ];
+            // default:
+            //     return tuits
         }
 }
 
